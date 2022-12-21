99d65ad0d9e9b618bbb4ac95452417d0863cdb60
ali-haider-dev <alihaider061297@gmail.com>
2022-12-21 22:37:14 +0500

    .

--- src/Components/form/Form.js
+++ src/Components/form/Form.js
@@ -1,11 +1,27 @@
 import { TextField } from "@mui/material";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./Form.css";
 
 function Form() {
-  const [value, setValue] = useState(
-    "RS is said to be consistent if and only if  its structure and style are such that any changes to the requirements can be made easily while retaining the style and structureygqydyq  fduyq fdyqf dyq fdqyfdyuqfdqfdqfduqfdyfquoyfdqydfowyfy odfwyfdywqfdywd wydf ywdf wyfd yd  ydfwuoydfoyw   wdowdfwydfywfodwuyfdowdfowfyd owdywfdywfduwfydwdfwfdywyd dwdywfdwydfywdwqfdwdlld lyo yvvcywc wydydggoudywvyuvcyuuydgwy d   ydywdyqwdfy"
-  );
+  const [value, setValue] = useState("");
+const[answer,SetAnswer] = useState("")
+  useEffect(() => {
+  
+  }, [])
+  
+const getAnswer=()=>{
+  fetch(`https://demo.broadn.ai/newpickaxe/getformnew?formid=469JEWJX3MGXRQE4TOQL&uniqueid=0000`).then((res)=>{
+    res.json().then(response=>{
+      console.log("response>>>>>>>>>>",response);
+    }).catch(e=>{
+      console.log("eroor",e.message);
+    })
+    
+       }).catch((error)=>{
+        console.log(error.message)
+       })
+}
+
   return (
     <div className="form">
       <div className="form_main">
@@ -18,10 +34,11 @@ function Form() {
           <h1 className="form_h1">Pickaxe FAQ Receptionist</h1>
           <h5>Ask me anything about Pickaxe and the new beta!</h5>
           <div>
-            <input placeholder="How can I help?" />
-            <button className="Form_button">Ask</button>
+            <input placeholder="How can I help?" onChange={(e)=>setValue(e.target.value)}/>
+            <button className="Form_button" onClick={()=>(getAnswer())}>Ask</button>
           </div>
-          <textarea className="Form_answer" value={value} disabled></textarea>
+          {/* <textarea className="Form_answer" value={value} disabled></textarea> */}
+         {answer? <div className="Form_answer"></div>:""}
         </div>
 
       </div>
